86fd43f8e20b901249d72ce8ff952686da8a65ff
taiduc1001 <phantaiduc2005@gmail.com>
2024-09-01 14:40:16 +0700

    Chores: remove parse annot, display grid

--- models/utils.py
+++ models/utils.py
@@ -1,64 +1,10 @@
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
 import numpy as np
-import os
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-from tqdm import tqdm
 
 import torch
 from torchvision import ops
-import torch.nn.functional as F
-import torch.optim as optim
-
-# -------------- Data Untils -------------------
-
-def parse_annotation(annotation_path, image_dir, img_size):
-    '''
-    Traverse the xml tree, get the annotations, and resize them to the scaled image size
-    '''
-    img_h, img_w = img_size
-
-    with open(annotation_path, "r") as f:
-        tree = ET.parse(f)
-
-    root = tree.getroot()  
-    
-    img_paths = []
-    gt_boxes_all = []
-    gt_classes_all = []
-    # get image paths
-    for object_ in root.findall('image'):
-        img_path = os.path.join(image_dir, object_.get("name"))
-        img_paths.append(img_path)
-      
-        # get raw image size    
-        orig_w = int(object_.get("width"))
-        orig_h = int(object_.get("height"))
-            
-        # get bboxes and their labels   
-        groundtruth_boxes = []
-        groundtruth_classes = []
-        for box_ in object_.findall('box'):
-            xmin = float(box_.get("xtl"))
-            ymin = float(box_.get("ytl"))
-            xmax = float(box_.get("xbr"))
-            ymax = float(box_.get("ybr"))
-        
-            # rescale bboxes
-            bbox = torch.Tensor([xmin, ymin, xmax, ymax])
-            bbox[[0, 2]] = bbox[[0, 2]] * img_w/orig_w
-            bbox[[1, 3]] = bbox[[1, 3]] * img_h/orig_h
-        
-            groundtruth_boxes.append(bbox.tolist())
-
-            # get labels
-            label = box_.get("label")
-            groundtruth_classes.append(label)
-
-        gt_boxes_all.append(torch.Tensor(groundtruth_boxes))
-        gt_classes_all.append(groundtruth_classes)
-                
-    return gt_boxes_all, gt_classes_all, img_paths
 
 # -------------- Prepocessing utils ----------------
 
@@ -270,13 +216,14 @@ def get_req_anchors(anc_boxes_all, gt_bboxes_all, gt_classes_all, pos_thresh=0.7
 
 # # -------------- Visualization utils ----------------
 
-def display_img(img_data, fig, axes):
+def display_img(img_data, fig, ax):
     for i, img in enumerate(img_data):
         if type(img) == torch.Tensor:
-            img = img.permute(1, 2, 0).numpy()
-        axes[i].imshow(img)
+            print(img.shape)
+            img = img.permute(1, 2, 0).numpy().astype(np.uint8)
+        ax.imshow(img)
     
-    return fig, axes
+    return fig, ax
 
 def display_bbox(bboxes, fig, ax, classes=None, in_format='xyxy', color='y', line_width=3):
     if type(bboxes) == np.ndarray:
@@ -297,18 +244,5 @@ def display_bbox(bboxes, fig, ax, classes=None, in_format='xyxy', color='y', lin
                 continue
             ax.text(x + 5, y + 20, classes[c], bbox=dict(facecolor='yellow', alpha=0.5))
         c += 1
-        
-    return fig, ax
-
-def display_grid(x_points, y_points, fig, ax, special_point=None):
-    # plot grid
-    for x in x_points:
-        for y in y_points:
-            ax.scatter(x, y, color="w", marker='+')
-            
-    # plot a special point we want to emphasize on the grid
-    if special_point:
-        x, y = special_point
-        ax.scatter(x, y, color="red", marker='+')
         
     return fig, ax
